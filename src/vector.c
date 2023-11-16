c48ffdd7b3605224e3a24727566b0d8c936fccb7
Louie-KC <83757145+Louie-KC@users.noreply.github.com>
2023-11-16 20:47:24 +1100

    Revise vector typedefs and function headers. Implement vector 4

--- src/vector.c
+++ src/vector.c
@@ -1,115 +1,155 @@
 #include <math.h>
+#include "vector.h"
 
-typedef struct vector2f {
-    float x;
-    float y;
-} vector2f;
+vec2 vec2_zero() {
+    vec2 result = {0.0f, 0.0f};
+    return result;
+}
 
-typedef struct vector3f {
-    float x;
-    float y;
-    float z;
-} vector3f;
-
-typedef struct vector4f {
-    float x;
-    float y;
-    float z;
-    float w;
-} vector4f;
-
-void reset_vec2f(vector2f *vec) {
+void vec2_set_zero(vec2 *vec) {
     vec->x = 0.0f;
     vec->y = 0.0f;
 }
 
-void reset_vec3f(vector3f *vec) {
+void vec2_negate(vec2 *vec) { 
+    vec->x *= -1.0f;
+    vec->y *= -1.0f;
+}
+
+void vec2_add(vec2 *dest, vec2 *with) {
+    dest->x += with->x;
+    dest->y += with->y;
+}
+
+void vec2_sub(vec2 *dest, vec2 *with) {
+    dest->x -= with->x;
+    dest->y -= with->y;
+}
+
+float vec2_magnitude(vec2 vec) {
+    float x = vec.x;
+    float y = vec.y;
+    return (float) sqrt(x*x + y*y);
+}
+
+void vec2_normalise(vec2 *vec) {
+    float magnitude = vec2_magnitude(*vec);
+    vec->x /= magnitude;
+    vec->y /= magnitude;
+}
+
+float vec2_dot_product(vec2 a, vec2 b) {
+    float result = 0.0f;
+    result += a.x * b.x;
+    result += a.y * b.y;
+    return result;
+}
+
+vec3 vec3_zero() {
+    vec3 result = {0.0f, 0.0f, 0.0f};
+    return result;
+}
+
+void vec3_set_zero(vec3 *vec) {
     vec->x = 0.0f;
     vec->y = 0.0f;
     vec->z = 0.0f;
 }
 
-void reset_vec4f(vector4f *vec) {
+void vec3_negate(vec3 *vec) {
+    vec->x *= -1.0f;
+    vec->y *= -1.0f;
+    vec->z *= -1.0f;
+}
+
+void vec3_add(vec3 *dest, vec3 *with) {
+    dest->x += with->x;
+    dest->y += with->y;
+    dest->z += with->z;
+}
+
+void vec3_sub(vec3 *dest, vec3 *with) {
+    dest->x -= with->x;
+    dest->y -= with->y;
+    dest->z -= with->z;
+}
+
+float vec3_magnitude(vec3 vec) {
+    float x = vec.x;
+    float y = vec.y;
+    float z = vec.z;
+    return (float) sqrt(x*x + y*y + z*z);
+}
+
+void vec3_normalise(vec3 *vec) {
+    float magnitude = vec3_magnitude(*vec);
+    vec->x /= magnitude;
+    vec->y /= magnitude;
+    vec->z /= magnitude;
+}
+
+float vec3_dot_product(vec3 a, vec3 b) {   
+    float result = 0;
+    result += a.x * b.x;
+    result += a.y * b.y;
+    result += a.z * b.z;
+    return result;
+}
+
+vec4 vec4_zero() {
+    vec4 result = {0.0f, 0.0f, 0.0f, 0.0f};
+    return result;
+}
+
+void vec4_set_zero(vec4 *vec) {
     vec->x = 0.0f;
     vec->y = 0.0f;
     vec->z = 0.0f;
     vec->w = 0.0f;
 }
 
-void negate_vec2f(vector2f *vec) { 
-    vec->x *= -1.0f;
-    vec->y *= -1.0f;
-}
-
-void negate_vec3f(vector3f *vec) {
+void vec4_negate(vec4 *vec) {
     vec->x *= -1.0f;
     vec->y *= -1.0f;
     vec->z *= -1.0f;
+    vec->w *= -1.0f;
 }
 
-float magnitude_vec2f(vector2f vec) {
-    float x = vec.x;
-    float y = vec.y;
-    return (float) sqrt(x*x + y*y);
+void vec4_add(vec4 *dest, vec4 *with) {
+    dest->x += with->x;
+    dest->y += with->y;
+    dest->z += with->z;
+    dest->w += with->w;
 }
 
-float magnitude_vec3f(vector3f vec) {
+void vec4_sub(vec4 *dest, vec4 *with) {
+    dest->x -= with->x;
+    dest->y -= with->y;
+    dest->z -= with->z;
+    dest->w -= with->w;
+}
+
+float vec4_magnitude(vec4 vec) {
     float x = vec.x;
     float y = vec.y;
     float z = vec.z;
-    return (float) sqrt(x*x + y*y + z*z); 
+    float w = vec.w;
+    return (float) sqrt(x*x + y*y + z*z + w*w);
 }
 
-void normalise_vec2f(vector2f *vec) {
-    float magnitude = magnitude_vec2f(*vec);
+void vec4_normalise(vec4 *vec) {
+    float magnitude = vec4_magnitude(*vec);
     vec->x /= magnitude;
     vec->y /= magnitude;
+    vec->z /= magnitude;
+    vec->w /= magnitude;
 }
 
-void normalise_vec3f(vector3f *vec) {
-    float magnitude = magnitude_vec3f(*vec);
-    vec->x /= magnitude; 
-    vec->y /= magnitude; 
-    vec->z /= magnitude; 
-}
-
-vector2f add_vec2f(vector2f a, vector2f b) {
-    a.x += b.x;
-    a.y += b.y;
-    return a;
-}
-
-vector3f add_vec3f(vector3f a, vector3f b) {
-    a.x += b.x;
-    a.y += b.y;
-    a.z += b.z;
-    return a;
-}
-
-vector2f sub_vec2f(vector2f a, vector2f b) {
-    a.x -= b.x;
-    a.y -= b.y;
-    return a;
-}
-
-vector3f sub_vec3f(vector3f a, vector3f b) {
-    a.x -= b.x;
-    a.y -= b.y;
-    a.z -= b.z;
-    return a;
-}
-
-float dot_product_vec2f(vector2f a, vector3f b) {
-    float result = 0;
-    result += a.x * b.x;
-    result += a.y * b.y;
-    return result;
-}
-
-float dot_product_vec3f(vector3f a, vector3f b) {   
+float vec4_dot_product(vec4 a, vec4 b) {
     float result = 0;
     result += a.x * b.x;
     result += a.y * b.y;
     result += a.z * b.z;
+    result += a.w * b.w;
     return result;
 }
---
+++ src/vector.h
@@ -0,0 +1,48 @@
+#pragma once
+typedef struct vector2f {
+    float x;
+    float y;
+} vec2;
+
+#pragma once
+typedef struct vector3f {
+    float x;
+    float y;
+    float z;
+} vec3;
+
+#pragma once
+typedef struct vector4f {
+    float x;
+    float y;
+    float z;
+    float w;
+} vec4;
+
+vec2 vec2_zero();
+void vec2_set_zero(vec2 *vec);
+void vec2_negate(vec2 *vec);
+void vec2_add(vec2 *dest, vec2 *with);
+void vec2_sub(vec2 *dest, vec2 *with);
+float vec2_magnitude(vec2 vec);
+void vec2_normalise(vec2 *vec);
+float vec2_dot_product(vec2 a, vec2 b);
+
+vec3 vec3_zero();
+void vec3_set_zero(vec3 *vec);
+void vec3_negate(vec3 *vec);
+void vec3_add(vec3 *dest, vec3 *with);
+void vec3_sub(vec3 *dest, vec3 *with);
+float vec3_magnitude(vec3 vec);
+void vec3_normalise(vec3 *vec);
+float vec3_dot_product(vec3 a, vec3 b);
+
+vec4 vec4_zero();
+void vec4_set_zero(vec4 *vec);
+void vec4_negate(vec4 *vec);
+void vec4_add(vec4 *dest, vec4 *with);
+void vec4_sub(vec4 *dest, vec4 *with);
+float vec4_magnitude(vec4 vec);
+void vec4_normalise(vec4 *vec);
+float vec4_dot_product(vec4 a, vec4 b);
+
