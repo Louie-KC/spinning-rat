67658f38ef1d125b6f9c12113c3c135be36a1574
Louie-KC <83757145+Louie-KC@users.noreply.github.com>
2023-12-13 12:54:32 +1100

    new face struct type. scene object to use vec3 and face for mesh data

--- src/scene-object.c
+++ src/scene-object.c
@@ -4,6 +4,10 @@
 // Note: ASSIMP triangulation appears to only work when a single models faces
 //       are ALL of the same order.
 struct scene_object scn_obj_load(const char* file_path, unsigned int flags) {
+#ifdef SCN_OBJ_DEBUG
+    printf("\nscn_obj_load: %s\n", file_path);
+#endif
+
     scene_object object;
     object.model_matrix = mat4_identity();
     object.parent = NULL;
@@ -24,34 +28,34 @@ struct scene_object scn_obj_load(const char* file_path, unsigned int flags) {
 
         // At least 1 mesh present with AI_SCENE_FLAGS_INCOMPLETE not being set
 
-        // Calculate and allocate required memory for model. Each face is made
-        // up of 3 faces with the aiProcess_Triangulate flag.
-        object.vertex_buffer_len = 3 * scene->mMeshes[0]->mNumVertices;
-        object.index_buffer_len  = 3 * scene->mMeshes[0]->mNumFaces;
+        // Calculate and allocated the required memory for the models mesh.
+        object.vertex_buffer_len = scene->mMeshes[0]->mNumVertices;
+        object.index_buffer_len  = scene->mMeshes[0]->mNumFaces;
         for (mi = 1; mi < scene->mNumMeshes; mi++) {
-            object.vertex_buffer_len += 3 * scene->mMeshes[mi]->mNumVertices;
-            object.index_buffer_len  += 3 * scene->mMeshes[mi]->mNumFaces;
+            object.vertex_buffer_len += scene->mMeshes[mi]->mNumVertices;
+            object.index_buffer_len  += scene->mMeshes[mi]->mNumFaces;
         }
-        object.vertex_buffer  = malloc(object.vertex_buffer_len * sizeof(float));
-        object.normals_buffer = malloc(object.vertex_buffer_len * sizeof(float));
-        object.index_buffer   = malloc(object.index_buffer_len  * sizeof(unsigned int));
+        object.index_buffer_len *= 3;  // 3 indices are used to make up a face
+
+        object.vertex_buffer = malloc(object.vertex_buffer_len * sizeof(vec3));
+        object.normal_buffer = malloc(object.vertex_buffer_len * sizeof(vec3));
+        object.index_buffer  = malloc(object.index_buffer_len  * sizeof(face));
 
         vi = 0;
         fi = 0;
         for (mi = 0; mi < scene->mNumMeshes; mi++) {
             mesh = *scene->mMeshes[mi];
 
-            // vertices & normals & texture coords
+            // vertices & normals
             unsigned int cur_vi = 0;
             while (cur_vi < mesh.mNumVertices) {
-                object.vertex_buffer[vi * 3]     = mesh.mVertices[cur_vi].x;
-                object.vertex_buffer[vi * 3 + 1] = mesh.mVertices[cur_vi].y;
-                object.vertex_buffer[vi * 3 + 2] = mesh.mVertices[cur_vi].z;
+                object.vertex_buffer[vi].x = mesh.mVertices[cur_vi].x;
+                object.vertex_buffer[vi].y = mesh.mVertices[cur_vi].y;
+                object.vertex_buffer[vi].z = mesh.mVertices[cur_vi].z;
 
-
-                object.normals_buffer[vi * 3]     = mesh.mNormals[cur_vi].x;
-                object.normals_buffer[vi * 3 + 1] = mesh.mNormals[cur_vi].y;
-                object.normals_buffer[vi * 3 + 2] = mesh.mNormals[cur_vi].z;
+                object.normal_buffer[vi].x = mesh.mNormals[cur_vi].x;
+                object.normal_buffer[vi].y = mesh.mNormals[cur_vi].y;
+                object.normal_buffer[vi].z = mesh.mNormals[cur_vi].z;
                 
                 cur_vi++;
                 vi++;
@@ -64,10 +68,9 @@ struct scene_object scn_obj_load(const char* file_path, unsigned int flags) {
             unsigned int cur_fi = 0;
             while (cur_fi < mesh.mNumFaces) {
                 const struct aiFace face = mesh.mFaces[cur_fi];
-
-                object.index_buffer[fi * 3]     = face.mIndices[0];
-                object.index_buffer[fi * 3 + 1] = face.mIndices[1];
-                object.index_buffer[fi * 3 + 2] = face.mIndices[2];
+                object.index_buffer[fi].indices[0] = face.mIndices[0];
+                object.index_buffer[fi].indices[1] = face.mIndices[1];
+                object.index_buffer[fi].indices[2] = face.mIndices[2];
 
                 cur_fi++;
                 fi++;
@@ -85,7 +88,8 @@ struct scene_object scn_obj_load(const char* file_path, unsigned int flags) {
         // process flags (except for flip winding order)
         struct bounding_box bounds;
         if (flags & (SCN_OBJ_IMPORT_CENTRED | SCN_OBJ_IMPORT_UNIT_RESCALE)) {
-            bounds = bounding_box(object.vertex_buffer, object.vertex_buffer_len);
+            // vec3 is contiguous in memory, len * 3 to cover entire vec3 buffer
+            bounds = bounding_box((float*) object.vertex_buffer, object.vertex_buffer_len * 3);
         }
         if (flags & SCN_OBJ_IMPORT_CENTRED) {
             scn_obj_centre_model(object.vertex_buffer, object.vertex_buffer_len, bounds);
@@ -94,28 +98,23 @@ struct scene_object scn_obj_load(const char* file_path, unsigned int flags) {
             normalise_model_scale(object.vertex_buffer, object.vertex_buffer_len, bounds);
         }
         if (flags & SCN_OBJ_IMPORT_FLIP_NORMALS) {
-            invert_float_buffer(object.normals_buffer, object.vertex_buffer_len);
+            // vec3 is contiguous in memory, len * 3 to cover entire vec3 buffer
+            invert_float_buffer((float*) object.normal_buffer, object.vertex_buffer_len * 3);
         }
 
 #ifdef SCN_OBJ_DEBUG
         printf("n_vertices: %d\nn_indices:  %d\n", object.vertex_buffer_len, object.index_buffer_len);
         for (int i = 0; i < object.vertex_buffer_len; ++i) {
-            printf("%f ", (object.vertex_buffer)[i]);
-            if ((i + 1) % 3 == 0) {
-                printf("\n");
-            }
-        }
-        for (int i = 0; i < object.index_buffer_len; ++i) {
-            printf("%d ", (object.index_buffer)[i]);
-            if ((i + 1) % 3 == 0) {
-                printf("\n");
-            }
+            printf("v %f %f %f\n", object.vertex_buffer[i].x,
+                    object.vertex_buffer[i].y, object.vertex_buffer[i].z);
         }
         for (int i = 0; i < object.vertex_buffer_len; ++i) {
-            printf("%f ", (object.normals_buffer)[i]);
-            if ((i + 1) % 3 == 0) {
-                printf("\n");
-            }
+            printf("vn %f %f %f\n", object.normal_buffer[i].x,
+                    object.normal_buffer[i].y, object.normal_buffer[i].z);
+        }
+        for (int i = 0; i < object.index_buffer_len / 3; ++i) {
+            printf("f %u %u %u\n", object.index_buffer[i].indices[0],
+                    object.index_buffer[i].indices[1], object.index_buffer[i].indices[2]);
         }
         printf("\n");
 #endif
@@ -127,21 +126,21 @@ struct scene_object scn_obj_load(const char* file_path, unsigned int flags) {
     return object;
 }
 
-void scn_obj_centre_model(float *vertex_buffer, int n_vertices, struct bounding_box bounds) {
+void scn_obj_centre_model(vec3 *vertex_buffer, int n_vertices, struct bounding_box bounds) {
     float x_offset = (bounds.x_min + bounds.x_max) / 2.0f;
     float y_offset = (bounds.y_min + bounds.y_max) / 2.0f;
     float z_offset = (bounds.z_min + bounds.z_max) / 2.0f;
 
     // printf("x offset: %f\ny_offset: %f\nz_offset: %f\n", x_offset, y_offset, z_offset);
 
-    for (int i = 0; i < n_vertices / 3; i++) {
-        vertex_buffer[i * 3]     -= x_offset;
-        vertex_buffer[i * 3 + 1] -= y_offset;
-        vertex_buffer[i * 3 + 2] -= z_offset;
+    for (int i = 0; i < n_vertices; i++) {
+        vertex_buffer[i].x -= x_offset;
+        vertex_buffer[i].y -= y_offset;
+        vertex_buffer[i].z -= z_offset;
     }
 }
 
-void normalise_model_scale(float* vertex_buffer, int n_vertices, struct bounding_box bounds) {
+void normalise_model_scale(vec3* vertex_buffer, int n_vertices, struct bounding_box bounds) {
     float x_scale = 1.0f / (bounds.x_max - bounds.x_min);
     float y_scale = 1.0f / (bounds.y_max - bounds.y_min);
     float z_scale = 1.0f / (bounds.z_max - bounds.z_min);
@@ -154,10 +153,10 @@ void normalise_model_scale(float* vertex_buffer, int n_vertices, struct bounding
         scale = z_scale;
     }
 
-    for (int i = 0; i < n_vertices / 3; i++) {
-        vertex_buffer[i * 3]     *= scale;
-        vertex_buffer[i * 3 + 1] *= scale;
-        vertex_buffer[i * 3 + 2] *= scale;
+    for (int i = 0; i < n_vertices; i++) {
+        vertex_buffer[i].x *= scale;
+        vertex_buffer[i].y *= scale;
+        vertex_buffer[i].z *= scale;
     }
 }
 
@@ -174,22 +173,46 @@ void scn_obj_set_buffers(scene_object* scn_obj) {
 
     glBindVertexArray(scn_obj->VAO);
     glBindBuffer(GL_ARRAY_BUFFER, scn_obj->VBO);
-    int vertex_buffer_size = sizeof(float) * scn_obj->vertex_buffer_len;
+    int vertex_buffer_size = sizeof(vec3) * scn_obj->vertex_buffer_len;
     // allocate buffer for BOTH vertices and normals. Then load first half with vertices
-    // and other half with normals (batching).
+    // and second half with normals (batching).
     glBufferData(GL_ARRAY_BUFFER, vertex_buffer_size * 2, NULL, GL_STATIC_DRAW);
     glBufferSubData(GL_ARRAY_BUFFER, 0, vertex_buffer_size, scn_obj->vertex_buffer);
-    glBufferSubData(GL_ARRAY_BUFFER, vertex_buffer_size, vertex_buffer_size, scn_obj->normals_buffer);
-    
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, scn_obj->EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * scn_obj->index_buffer_len,
-                 scn_obj->index_buffer, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, vertex_buffer_size, vertex_buffer_size, scn_obj->normal_buffer);
+
+#ifdef SCN_OBJ_DEBUG
+    vec3 *buffer = malloc(vertex_buffer_size * 2);
+    glGetBufferSubData(GL_ARRAY_BUFFER, 0, vertex_buffer_size * 2, buffer);
+    printf("From gl buffer\n");
+    for (int i = 0; i < scn_obj->vertex_buffer_len; i++) {
+        printf("v %f %f %f\n", buffer[i].x, buffer[i].y, buffer[i].z);
+    }
+    for (int i = scn_obj->vertex_buffer_len; i < scn_obj->vertex_buffer_len * 2; i++) {
+        printf("vn %f %f %f\n", buffer[i].x, buffer[i].y, buffer[i].z);
+    }
+    free(buffer);
+#endif
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vec3), (void*)0);
 
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) (vertex_buffer_size+0L));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(vec3), (void*) (vertex_buffer_size+0L));
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, scn_obj->EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(face) * scn_obj->index_buffer_len,
+                 scn_obj->index_buffer, GL_STATIC_DRAW);
+
+#ifdef SCN_OBJ_DEBUG
+    unsigned int idx_buffer_byte_size = sizeof(face) * scn_obj->index_buffer_len;
+    face *idx_debug_buffer = malloc(idx_buffer_byte_size);
+    glGetBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, idx_buffer_byte_size, idx_debug_buffer);
+    for (int i = 0; i < scn_obj->index_buffer_len / 3; i++) {
+        printf("f %u %u %u\n", idx_debug_buffer[i].indices[0],
+                idx_debug_buffer[i].indices[1], idx_debug_buffer[i].indices[2]);
+    }
+    free(idx_debug_buffer);
+#endif
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);  // release VAO
--- src/scene-object.h
+++ src/scene-object.h
@@ -9,22 +9,30 @@
 #include <assimp/postprocess.h>
 
 #include "matrix.h"
+#include "vector.h"
 #include "util.h"
 
-// #define SCN_OBJ_DEBUG
+#define SCN_OBJ_DEBUG
 
 #define SCN_OBJ_IMPORT_CENTRED              0b10000000
 #define SCN_OBJ_IMPORT_UNIT_RESCALE         0b01000000
 #define SCN_OBJ_IMPORT_FLIP_NORMALS         0b00100000
 #define SCN_OBJ_IMPORT_FLIP_WINDING_ORDER   0b00010000
 
+typedef struct face {
+    unsigned int indices[3];
+} face;
+
 typedef struct scene_object {
     unsigned int VBO;
     unsigned int VAO;
     unsigned int EBO;
-    float* vertex_buffer;
-    float* normals_buffer;
-    unsigned int* index_buffer;
+    vec3 *vertex_buffer;
+    vec3 *normal_buffer;
+    // float* vertex_buffer;
+    // float* normals_buffer;
+    // unsigned int* index_buffer;
+    face *index_buffer;
     unsigned int vertex_buffer_len;
     unsigned int index_buffer_len;
     mat4 model_matrix;
@@ -35,12 +43,12 @@ struct scene_object scn_obj_load(const char* file_path, unsigned int flags);
 
 // Shift all vertices of a model/mesh to be centered at [0, 0, 0].
 // Assumes `bounds` is the result of model_bounding_box with the same model.
-void scn_obj_centre_model(float *vertex_buffer, int n_vertices, struct bounding_box bounds);
+void scn_obj_centre_model(vec3 *vertex_buffer, int n_vertices, struct bounding_box bounds);
 
 // Scale a model/mesh to be at most 2 unit lengths ([-1, 1] if centred).
 // NOTE: This removes any scale/length of unit lengths (metres, feet, etc) from mesh.
 // Assumes `bounds` is the result of model_bounding_box with the same model.
-void normalise_model_scale(float* vertex_buffer, int n_vertices, struct bounding_box bounds);
+void normalise_model_scale(vec3* vertex_buffer, int n_vertices, struct bounding_box bounds);
 
 // Creates, binds, and feeds data to a scene objects buffer objects.
 // Invokes deletion methods on the scene objects buffers objects prior
